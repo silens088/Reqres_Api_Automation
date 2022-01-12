31918006d8d354103ded438a15494601746077ff
Vitalii_Velich <silens088@gmail.com>
2022-01-12 23:29:19 +0300

    added откатил изменения. Не та репа в аллюр

--- src/test/java/test_base/TestBase.java
+++ src/test/java/test_base/TestBase.java
@@ -1,36 +1,36 @@
-package test_base;
-
-import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.logevents.SelenideLogger;
-import io.qameta.allure.selenide.AllureSelenide;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.openqa.selenium.remote.DesiredCapabilities;
-
-//пробую запустить тесты в аллюр
-//добавил расширение public class DemowebshopTests extends TestBase
-
-public class TestBase {
-
-    @BeforeAll
-    static void setup() {
-        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-        Configuration.startMaximized = true;
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
-
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("enableVNC", true);
-        capabilities.setCapability("enableVideo", true);
-
-        Configuration.browserCapabilities = capabilities;
-    }
-
-//    @AfterEach
-//    public void tearDown() {
-//        Attach.screenshotAs("Last screenshot");
-//        Attach.pageSource();
-//        Attach.browserConsoleLogs();
-//        Attach.addVideo();
+//package test_base;
+//
+//import com.codeborne.selenide.Configuration;
+//import com.codeborne.selenide.logevents.SelenideLogger;
+//import io.qameta.allure.selenide.AllureSelenide;
+//import org.junit.jupiter.api.AfterEach;
+//import org.junit.jupiter.api.BeforeAll;
+//import org.openqa.selenium.remote.DesiredCapabilities;
+//
+////пробую запустить тесты в аллюр
+////добавил расширение public class DemowebshopTests extends TestBase
+//
+//public class TestBase {
+//
+//    @BeforeAll
+//    static void setup() {
+//        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+//        Configuration.startMaximized = true;
+//        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
+//
+//        DesiredCapabilities capabilities = new DesiredCapabilities();
+//        capabilities.setCapability("enableVNC", true);
+//        capabilities.setCapability("enableVideo", true);
+//
+//        Configuration.browserCapabilities = capabilities;
 //    }
-}
+//
+////    @AfterEach
+////    public void tearDown() {
+////        Attach.screenshotAs("Last screenshot");
+////        Attach.pageSource();
+////        Attach.browserConsoleLogs();
+////        Attach.addVideo();
+////    }
+//}
 
